017db4e0737a327532e26f0785674fee657545f5
CARRE BEATRICE <beatrice.carre@gmail.com>
2015-01-28 14:07:23 +0100

    def

--- definitions.v
+++ definitions.v
@@ -3,52 +3,147 @@ Require Import Arith.
 Definition var := nat.
 
 Inductive trm : Set :=
-  | trm_bvar : nat -> trm
-  | trm_fvar : var -> trm
-  | trm_app  : trm -> trm -> trm
-  | trm_abs  : trm -> trm.
+| trm_bvar : nat -> trm
+| trm_fvar : var -> trm
+| trm_app  : trm -> trm -> trm
+| trm_abs  : trm -> trm.
 
 (* u is the fresh variable name we use to open t 
    k is the current depth *)
-Fixpoint open_rec (k : nat) (u : trm) (t : trm) : trm :=
+Fixpoint open_rec (k : nat) (x : var) (t : trm) : trm :=
   match t with
-  | trm_bvar i    => if (beq_nat k i) then u else (trm_bvar i)
-  | trm_fvar x    => trm_fvar x 
-  | trm_app t1 t2 => trm_app (open_rec k u t1) (open_rec k u t2)
-  | trm_abs t1    => trm_abs (open_rec (S k) u t1) 
+    | trm_bvar i    => if (beq_nat k i) then trm_fvar x else (trm_bvar i)
+    | trm_fvar y    => trm_fvar y 
+    | trm_app t1 t2 => trm_app (open_rec k x t1) (open_rec k x t2)
+    | trm_abs t1    => trm_abs (open_rec (S k) x t1) 
   end.
 
-Definition open t u := open_rec 0 u t.
+Definition open t x := open_rec 0 x t.
 
-Fixpoint close_var_rec (k : nat) (z : var) (t : trm) : trm :=
+
+Fixpoint close_var_rec (k : nat) (x : var) (t : trm) : trm :=
   match t with
-  | trm_bvar i    => trm_bvar i 
-  | trm_fvar x    => if beq_nat x z then (trm_bvar k) else (trm_fvar x)
-  | trm_app t1 t2 => trm_app (close_var_rec k z t1) (close_var_rec k z t2)
-  | trm_abs t1    => trm_abs (close_var_rec (S k) z t1) 
+    | trm_bvar i    => trm_bvar i 
+    | trm_fvar y    => if beq_nat x y then (trm_bvar k) else (trm_fvar y)
+    | trm_app t1 t2 => trm_app (close_var_rec k x t1) (close_var_rec k x t2)
+    | trm_abs t1    => trm_abs (close_var_rec (S k) x t1) 
   end.
 
+Definition close_var x t := close_var_rec 0 x t.
 
-Definition close_var z t := close_var_rec 0 z t.
-
-Fixpoint locally_closed_rec (k : nat) (t : trm) : Prop :=
+Fixpoint locally_closed_at_rec (k : nat) (t : trm) : Prop :=
   match t with
-  | trm_bvar i    => i < k
-  | trm_fvar x    => True
-  | trm_app t1 t2 => (locally_closed_rec k t1) /\ (locally_closed_rec k t2)
-  | trm_abs t1    => (locally_closed_rec (S k) t1)
+    | trm_bvar i    => i < k
+    | trm_fvar x    => True
+    | trm_app t1 t2 => (locally_closed_at_rec k t1) /\ (locally_closed_at_rec k t2)
+    | trm_abs t1    => (locally_closed_at_rec (S k) t1)
   end.
 
-Lemma lc_app_k:
-  forall c:nat, forall t1:trm, forall t2:trm, locally_closed_rec c (trm_app t1 t2) -> (locally_closed_rec c t1) /\ (locally_closed_rec c t2).
+Definition locally_closed (t:trm) : Prop := locally_closed_at_rec 0 t.
+
+
+Lemma lc_at_app:
+  forall c:nat, forall t1:trm, forall t2:trm, locally_closed_at_rec c (trm_app t1 t2) -> (locally_closed_at_rec c t1) /\ (locally_closed_at_rec c t2).
 Proof.
   intros.
   simpl in H.
   exact H.
 Qed.
 
-Lemma lc_at0:
-  forall c:nat, forall t:trm, locally_closed_rec 0 t -> locally_closed_rec c t.
+
+Lemma close_open_var :
+  forall t:trm, forall k:nat, exists u:var, (close_var_rec k u (open_rec k u t)) = t.
+Proof.
+  intros.
+  induction t.
+  - induction k.
+    
+    
+  (*   induction n. *)
+  (*   case (n=0). *)
+  (*   simpl. *)
+  (*   assert (forall u0:var, beq_nat u0 u0 = true).   *)
+  (*   intros. *)
+  (*   induction u0.  *)
+  (*   reflexivity. *)
+  (*   simpl. *)
+  (*   exact IHu0. *)
+    
+
+
+    
+  (*   induction k. *)
+  (*   simpl. *)
+  (*   induction n. *)
+  (*   simpl. *)
+  (*   assert (forall u:var, beq_nat u u = true).   *)
+  (*   intros. *)
+  (*   induction u0.  *)
+  (*   reflexivity. *)
+  (*   simpl. *)
+  (*   exact IHu0. *)
+  (*   rewrite H. *)
+  (*   reflexivity. *)
+  (*   simpl. *)
+  (*   reflexivity. *)
+  (*   induction n. *)
+  (*   simpl. *)
+  (*   reflexivity. *)
+  (*   simpl. *)
+  (*   simpl. *)
+
+
+
+
+
+
+
+
+  (*   induction t. *)
+  (*   induction u. *)
+  (*   induction n.  *)
+  (* -simpl. *)
+  (*  simpl. *)
+  (*  reflexivity. *)
+  (* - simpl. *)
+  (*   reflexivity. *)
+  (* - destruct n. *)
+  (*   simpl. *)
+  (*   assert (forall u:var, beq_nat u u = true).   *)
+  (*   intros. *)
+  (*   induction u0. *)
+    
+  (*   reflexivity. *)
+  (*   simpl. *)
+  (*   exact IHu0. *)
+  (*   rewrite H. *)
+  (*   reflexivity. *)
+  (*   simpl. *)
+  (*   reflexivity.     *)
+  (* -  *)
+
+
+  (*   simpl close_var_rec. *)
+  (*   induction v. *)
+  (*   induction u. *)
+  (*   simpl. *)
+  (*   simpl      *)
+
+
+
+Qed.
+
+Lemma open_close_var :
+  forall u:var, forall t:trm, (open_rec 0 u (close_var_rec 0 u t)) = t.
+Proof.
+  intros.
+  
+Qed.
+
+
+
+Lemma lc_from_lc_at:
+  forall c:nat, forall t:trm, locally_closed_at_rec 0 t -> locally_closed c t.
 Proof.
 (*   intros. *)
 (*   induction c. *)
@@ -82,11 +177,11 @@ Proof.
 (*   intros. *)
 (*   exact H1. *)
 (* -  *)
-   
   
   
-    
-   
+  
+  
+  
 Qed.
 
 Definition locally_closed t := locally_closed_rec 0 t.
